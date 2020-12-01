2f037ea6c0070f9aca73e04796872cea86a43278
Anderson0202 <53060401+Anderson0202@users.noreply.github.com>
2020-12-01 17:05:29 -0500

    Agregar archivos de proyecto.

---
+++ Presentacion/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Presentacion
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection con = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string strConexion = "Data Source=.\\sqlexpress;" +
+            "Initial Catalog=bd_telefonos; Integrated Security=True";
+            con = new SqlConnection(strConexion);
+        }
+    }
+}
